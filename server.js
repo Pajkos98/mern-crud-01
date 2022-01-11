1921bac69a3f45395d98e1447f939994aa20863e
Pajkos98 <lmark9804@gmail.com>
2022-01-11 11:37:11 +0100

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-mongoose.connect('mongodb+srv://petruszka:pepe1973@cluster0.tmbwc.mongodb.net/mern-crud?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Pajkos98:Pajkos98@cluster0.suln3.mongodb.net/mern-crud-01?retryWrites=true&w=majority')
     .then(() => {
         console.log('Sikeres csatlakozás az adatbázishoz!');
     })
